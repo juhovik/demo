2f86c88816ba20e38d617e24fafdd867b490f337
Juho Vikman <c0viju00@students.oamk.fi>
2022-01-19 10:28:36 +0200

    testis

---
+++ App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [age, setAge] = useState(0);
+  const [lower, setLower] = useState(initialState)
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
