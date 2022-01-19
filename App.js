477587b24d19ab7972e680af1f004529f784af6c
Juho Vikman <c0viju00@students.oamk.fi>
2022-01-19 10:39:28 +0200

    valmis

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [age, setAge] = useState(0);
-  const [lower, setLower] = useState(initialState)
+  const [lower, setLower] = useState(0);
+  const [upper, setUpper] = useState(0);
+
+  function calculate() {
+    const lowerLimit = (220-age) * 0.65
+    setLower(lowerLimit);
+
+    const upperLimit = (220-age) * 0.95
+    setUpper(upperLimit);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Age</Text>
+      <TextInput
+      keyboardType='numeric'
+      value={age}
+      onChangeText={setAge}
+      >
+      </TextInput>
+      <Text>Limitis</Text>
+      <Text>{lower.toFixed(0)} - {upper.toFixed(0)}</Text>
+      <Button onPress={calculate} title='Calculate' />
     </View>
   );
 }
